b691ffc8ba50427c209b6a5aba8c764ed692d7f1
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-08-18 00:10:51 +0100

    Values of form saved in Dictionary

--- Session-2/ERPSolution/HRISWebApplication/Setup/CompanyInformationForm.aspx.cs
+++ Session-2/ERPSolution/HRISWebApplication/Setup/CompanyInformationForm.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,7 +17,31 @@ namespace HRISWebApplication.Setup
 
         protected void btnSave_Click(object sender, EventArgs e)
         {
+            SaveCompanyInformation();
+        }
 
+        private void SaveCompanyInformation()
+        {
+            IDictionary<string, string> companyDetails = new Dictionary<string, string>()
+            {
+                { "companyId", txtCompanyID.Text },
+                { "companyName", txtCompanyName.Text },
+                { "address1", txtAddress1.Text },
+                { "address2", txtAddress2.Text },
+                { "address3", txtAddress3.Text },
+                { "contactPersonAddress", txtContactPersonAddress.Text },
+                { "contactPersonEmail", txtContactPersonEmail.Text },
+                { "contactPersonPhoneNo", txtPhoneNumber.Text },
+                { "fax", txtFax.Text },
+                { "email", txtEmail.Text },
+                { "url", txtURL.Text },
+                { "tin", txtTin.Text },
+                { "regNo", txtRegNo.Text },
+                { "vatNo", txtVatNo.Text },
+                { "insurance", txtInsurance.Text }
+            };
+
+            throw new NotImplementedException();
         }
     }
 }
