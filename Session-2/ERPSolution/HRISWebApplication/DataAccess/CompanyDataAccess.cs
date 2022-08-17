262f7bd64ca4e517c1e5b06e6866bd86319c78f7
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-08-18 00:14:38 +0100

    CompanyDataAccess Added with SQL Connection

---
+++ Session-2/ERPSolution/HRISWebApplication/DataAccess/CompanyDataAccess.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace HRISWebApplication.DataAccess
+{
+    public class CompanyDataAccess
+    {
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionStringCompany"].ConnectionString);
+        
+    }
+}
