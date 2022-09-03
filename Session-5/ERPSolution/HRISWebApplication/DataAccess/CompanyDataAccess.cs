ea13a2b209fa7e3cc896813f7a1b81dbf7da997c
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-09-03 01:57:08 +0100

    Update Method Added

--- Session-5/ERPSolution/HRISWebApplication/DataAccess/CompanyDataAccess.cs
+++ Session-5/ERPSolution/HRISWebApplication/DataAccess/CompanyDataAccess.cs
@@ -37,6 +37,7 @@ namespace HRISWebApplication.DataAccess
         {
 
             _conn.Open();
+
             string sqlQuery = $"INSERT INTO [dbo].[Hrms_Company_Master] " +
                 $"([CompanyName] ,[CompanyId], [Address1] ,[Address2], [Address3],[ContPer1], [ContPer2], [Phone1]" +
                 $", [Fax1], [Email1] ,[Url1] ,[TIN],[RegNo] ,[VATNo] ,[Insurance1])" +
@@ -53,12 +54,24 @@ namespace HRISWebApplication.DataAccess
 
             try
             {
-                command.ExecuteNonQuery();
+                if (companyDetails["companyId"] != "")
+                    command.ExecuteNonQuery();
+                else
+                    throw new Exception();
             }
             catch(Exception ex)
             {
-                string primaryKeyExceptionMessage = "Duplicate Primary Key Entered";
-                HttpContext.Current.Response.Write($"<script>alert('{primaryKeyExceptionMessage}')</script>"); 
+                if (companyDetails["companyId"] == "")
+                {
+                    string primaryKeyExceptionMessage = "Empty Primary Key entered";
+                    HttpContext.Current.Response.Write($"<script>alert('{primaryKeyExceptionMessage}')</script>");
+                }
+                else
+                {
+                    string primaryKeyExceptionMessage = "Duplicate Primary Key Entered";
+                    HttpContext.Current.Response.Write($"<script>alert('{primaryKeyExceptionMessage}')</script>");
+                }
+                
             }
 
             _conn.Close();
@@ -68,6 +81,21 @@ namespace HRISWebApplication.DataAccess
         {
             _conn.Open();
             string sqlQuery = $"DELETE FROM [dbo].[Hrms_Company_Master] WHERE CompanyId='{companyId}'";
+            SqlCommand command = new SqlCommand(sqlQuery, _conn);
+            command.ExecuteNonQuery();
+            _conn.Close();
+        }
+
+        public void Update(IDictionary<string, string> companyDetails)
+        {
+            _conn.Open();
+            string sqlQuery = $@"UPDATE [dbo].[Hrms_Company_Master] Set CompanyName = '{companyDetails["companyName"]}',
+                               Address1 = '{companyDetails["address1"]}', Address2 = '{companyDetails["address2"]}', Address3 = '{companyDetails["address3"]}',
+                               ContPer1 = '{companyDetails["contactPersonAddress"]}', ContPer2 = '{companyDetails["contactPersonEmail"]}', Phone1 = '{companyDetails["contactPersonPhoneNo"]}',
+                               Fax1 = '{companyDetails["fax"]}', Email1 = '{companyDetails["email"]}', Url1 = '{companyDetails["url"]}', TIN = '{companyDetails["tin"]}',
+                               RegNo = '{companyDetails["regNo"]}', VATNo = '{companyDetails["vatNo"]}', Insurance1 = '{companyDetails["insurance"]}'
+                               WHERE CompanyId = '{companyDetails["companyId"]}'";
+
             SqlCommand command = new SqlCommand(sqlQuery, _conn);
             command.ExecuteNonQuery();
             _conn.Close();
