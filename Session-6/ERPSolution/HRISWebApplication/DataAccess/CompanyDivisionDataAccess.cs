ab3f33111b2d69bcd7ffefee44ef78fbe51b42b6
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-09-06 00:12:42 +0100

    CompanyDivisionDataAccess Initialized

---
+++ Session-6/ERPSolution/HRISWebApplication/DataAccess/CompanyDivisionDataAccess.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace HRISWebApplication.DataAccess
+{
+    public class CompanyDivisionDataAccess
+    {
+        private readonly SqlConnection _conn;
+
+        public CompanyDivisionDataAccess()
+        {
+            _conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionStringHRIS"].ConnectionString);
+        }
+    }
+}
