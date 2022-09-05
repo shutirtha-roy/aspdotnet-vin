dd0b7bdbf562a97fdb62809a28134fad2a9ee2cd
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-09-06 00:29:55 +0100

    GetAllCompanyDivisionInformation Added

--- Session-6/ERPSolution/HRISWebApplication/DataAccess/CompanyDivisionDataAccess.cs
+++ Session-6/ERPSolution/HRISWebApplication/DataAccess/CompanyDivisionDataAccess.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Data;
 using System.Data.SqlClient;
+using System.Configuration;
 
 namespace HRISWebApplication.DataAccess
 {
@@ -14,6 +15,20 @@ namespace HRISWebApplication.DataAccess
         public CompanyDivisionDataAccess()
         {
             _conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionStringHRIS"].ConnectionString);
+        }
+
+        public DataTable GetAllCompanyDivisionInformation()
+        {
+            _conn.Open();
+            string sqlQuery = @"SELECT [CompanyId], [OfficeLocationCode], [OfficeLocationName], 
+                [Location], [Address1], [Address2], [Address3]
+                FROM [dbo].[Hrms_Company_Division_Master]";
+            SqlCommand cmd = new SqlCommand(sqlQuery, _conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            DataTable dataTable = new DataTable();
+            dataTable.Load(reader);
+            _conn.Close();
+            return dataTable;
         }
     }
 }
