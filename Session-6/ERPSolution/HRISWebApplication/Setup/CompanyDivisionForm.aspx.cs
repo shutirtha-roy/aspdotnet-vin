050a3295407807e1b07e8f866f98d672c30b1602
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-09-06 00:29:33 +0100

    Show data in table

--- Session-6/ERPSolution/HRISWebApplication/Setup/CompanyDivisionForm.aspx.cs
+++ Session-6/ERPSolution/HRISWebApplication/Setup/CompanyDivisionForm.aspx.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using HRISWebApplication.DataAccess;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +11,15 @@ namespace HRISWebApplication.Setup
 {
     public partial class CompanyDivisionForm : System.Web.UI.Page
     {
+        private readonly CompanyDivisionDataAccess _companyDataAccess;
+        public CompanyDivisionForm()
+        {
+            _companyDataAccess = new CompanyDivisionDataAccess();
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            ShowCompanyDivisionInformation();
         }
 
         protected void btnSave_Click(object sender, EventArgs e)
@@ -27,6 +35,13 @@ namespace HRISWebApplication.Setup
         protected void GridCompanyDivision_RowCommand(object sender, GridViewCommandEventArgs e)
         {
 
+        }
+
+        private void ShowCompanyDivisionInformation()
+        {
+            DataTable dt = _companyDataAccess.GetAllCompanyDivisionInformation();
+            GridCompanyDivision.DataSource = dt;
+            GridCompanyDivision.DataBind();
         }
     }
 }
