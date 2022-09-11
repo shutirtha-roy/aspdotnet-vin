fc0bd007c24dff92cd750d05ec1a4d10e8731643
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-09-12 02:59:29 +0600

    DataAccess Added

---
+++ Session-6/ERPSolution/HRISWebApplication/DataAccess/DataAccess.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+namespace HRISWebApplication.DataAccess
+{
+    public class DataAccess
+    {
+        protected readonly SqlConnection _conn;
+        public DataAccess()
+        {
+            _conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionStringHRIS"].ConnectionString);
+        }
+    }
+}
