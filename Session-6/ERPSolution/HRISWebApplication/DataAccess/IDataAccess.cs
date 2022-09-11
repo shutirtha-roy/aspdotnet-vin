6b25ccf2797c4b5fa47cdefca103f638f8971715
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-09-12 03:11:47 +0600

    IDataAccess Interface Added

---
+++ Session-6/ERPSolution/HRISWebApplication/DataAccess/IDataAccess.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRISWebApplication.DataAccess
+{
+    public interface IDataAccess
+    {
+        void Save(IDictionary<string, string> companyDivisionDetails);
+        void Update(IDictionary<string, string> companyDetails);
+        void DeleteRow(string officeLocationCode);
+    }
+}
