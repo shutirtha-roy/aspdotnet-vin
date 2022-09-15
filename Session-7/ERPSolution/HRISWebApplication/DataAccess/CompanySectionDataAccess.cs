80033572df473abefcfbafe92206d566cc348ecf
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-09-16 00:40:44 +0600

    GetInformation and Save Implemented

--- Session-7/ERPSolution/HRISWebApplication/DataAccess/CompanySectionDataAccess.cs
+++ Session-7/ERPSolution/HRISWebApplication/DataAccess/CompanySectionDataAccess.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -12,19 +14,57 @@ namespace HRISWebApplication.DataAccess
                 
         }
 
+        public DataTable GetAllCompanySectionInformation()
+        {
+            _conn.Open();
+            string sqlQuery = @"SELECT [CompanyId], [OfficeLocationId], [DepartmentCode], 
+                [SectionCode], [SectionName], [HeadOfSection], [SubstituteHeadOfSection]
+                FROM [dbo].[Hrms_Company_Section_Master]";
+            SqlCommand cmd = new SqlCommand(sqlQuery, _conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            DataTable dataTable = new DataTable();
+            dataTable.Load(reader);
+            _conn.Close();
+            return dataTable;
+        }
+
         public void DeleteRow<T>(T officeLocationCode)
         {
             throw new NotImplementedException();
         }
 
-        public void Save(IDictionary<string, string> companyDivisionDetails)
+        public void Save(IDictionary<string, string> companySection)
         {
-            throw new NotImplementedException();
+            _conn.Open();
+
+            string sqlQuery = $@"INSERT INTO [dbo].[Hrms_Company_Section_Master] 
+                ([CompanyId] ,[OfficeLocationId], [DepartmentCode] ,[SectionCode]
+                , [SectionName], [HeadOfSection] ,[SubstituteHeadOfSection]) VALUES 
+                ('{companySection["companyId"]}', '{companySection["locationId"]}',
+                '{companySection["departmentCode"]}', '{companySection["sectionCode"]}',
+                '{companySection["sectionName"]}', '{companySection["headOfSection"]}',
+                '{companySection["substituteHeadOfSection"]}')";
+
+            SqlCommand command = new SqlCommand(sqlQuery, _conn);
+
+            try
+            {
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                string primaryKeyExceptionMessage = "Duplicate Primary Key Entered";
+                HttpContext.Current.Response.Write($"<script>alert('{primaryKeyExceptionMessage}')</script>");
+            }
+
+            _conn.Close();
         }
 
         public void Update(IDictionary<string, string> companyDetails)
         {
             throw new NotImplementedException();
         }
+
+        
     }
 }
