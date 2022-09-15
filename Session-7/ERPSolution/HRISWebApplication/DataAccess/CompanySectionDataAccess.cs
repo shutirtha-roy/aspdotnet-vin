cea861d288c1af0d1e8e30332d0b67267d348679
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-09-16 00:03:44 +0600

    CompanySectionDataAccess Added

---
+++ Session-7/ERPSolution/HRISWebApplication/DataAccess/CompanySectionDataAccess.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HRISWebApplication.DataAccess
+{
+    public class CompanySectionDataAccess : DataAccess
+    {
+        public CompanySectionDataAccess()
+        {
+                
+        }
+
+        public void DeleteRow<T>(T officeLocationCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(IDictionary<string, string> companyDivisionDetails)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(IDictionary<string, string> companyDetails)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
