2a784c569b9da7f2e50c8458553ef20239bf446d
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-10-07 01:41:45 +0600

    Session-11 Initialized

---
+++ Session-11/ERPSolution/HRISWebApplication/DataAccess/CompanyDesignationDataAccess.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace HRISWebApplication.DataAccess
+{
+    public class CompanyDesignationDataAccess : DataAccess
+    {
+        public CompanyDesignationDataAccess()
+        {
+
+        }
+
+        public DataTable GetAllCompanyDesignationInformation()
+        {
+            _conn.Open();
+            string sqlQuery = @"SELECT [CompanyId], [OfficeLocationId], [DepartmentCode], 
+                [SectionCode], [DesignationCode], [DesignationName] FROM [dbo].[Hrms_Company_Designations_Master]";
+            SqlCommand cmd = new SqlCommand(sqlQuery, _conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            DataTable dataTable = new DataTable();
+            dataTable.Load(reader);
+            _conn.Close();
+            return dataTable;
+        }
+
+        public void DeleteRow<T>(T designationCode)
+        {
+            _conn.Open();
+            string sqlQuery = $"DELETE FROM [dbo].[Hrms_Company_Designations_Master] WHERE DesignationCode='{designationCode}'";
+
+            SqlCommand command = new SqlCommand(sqlQuery, _conn);
+
+            try
+            {
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                string cascadingExceptionMessage = "Error in deleting due Cascading Relationship";
+                HttpContext.Current.Response.Write($"<script>alert('{cascadingExceptionMessage}')</script>");
+            }
+
+            _conn.Close();
+        }
+
+        public void Save(IDictionary<string, string> companyDesignation)
+        {
+            _conn.Open();
+
+            if (companyDesignation["companyId"] == "-1")
+            {
+                string companyExceptionMessage = "Company Id not selected";
+                HttpContext.Current.Response.Write($"<script>alert('{companyExceptionMessage}')</script>");
+            }
+            else if (companyDesignation["locationId"] == "-1")
+            {
+                string companyExceptionMessage = "Office Location Id not selected";
+                HttpContext.Current.Response.Write($"<script>alert('{companyExceptionMessage}')</script>");
+            }
+            else if (companyDesignation["departmentCode"] == "-1")
+            {
+                string companyExceptionMessage = "Department Code not selected";
+                HttpContext.Current.Response.Write($"<script>alert('{companyExceptionMessage}')</script>");
+            }
+            else if (companyDesignation["sectionCode"] == "-1" || companyDesignation["sectionCode"] == "")
+            {
+                string companyExceptionMessage = "Section Code not selected";
+                HttpContext.Current.Response.Write($"<script>alert('{companyExceptionMessage}')</script>");
+            }
+            else if (companyDesignation["designationCode"] == "-1" || companyDesignation["designationCode"] == "")
+            {
+                string companyExceptionMessage = "Designation Code not selected";
+                HttpContext.Current.Response.Write($"<script>alert('{companyExceptionMessage}')</script>");
+            }
+            else
+            {
+                string sqlQuery = $@"INSERT INTO [dbo].[Hrms_Company_Designations_Master] 
+                ([CompanyId] ,[OfficeLocationId], [DepartmentCode] ,[SectionCode], [DesignationCode]
+                , [DesignationName]) VALUES 
+                ('{companyDesignation["companyId"]}', '{companyDesignation["locationId"]}',
+                '{companyDesignation["departmentCode"]}', '{companyDesignation["sectionCode"]}',
+                '{companyDesignation["designationCode"]}', '{companyDesignation["designationName"]}')";
+
+                SqlCommand command = new SqlCommand(sqlQuery, _conn);
+
+                try
+                {
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    string primaryKeyExceptionMessage = "Duplicate Primary Key Entered";
+                    HttpContext.Current.Response.Write($"<script>alert('{primaryKeyExceptionMessage}')</script>");
+                }
+            }
+
+            _conn.Close();
+        }
+
+        public void Update(IDictionary<string, string> companyDesignation)
+        {
+            _conn.Open();
+            string sqlQuery = $@"UPDATE [dbo].[Hrms_Company_Designations_Master] Set CompanyId = '{companyDesignation["companyId"]}',
+                               OfficeLocationId = '{companyDesignation["locationId"]}', DepartmentCode = '{companyDesignation["departmentCode"]}',
+                               SectionCode = '{companyDesignation["sectionCode"]}',  DesignationName = '{companyDesignation["designationName"]}' 
+                               WHERE DesignationCode = '{companyDesignation["designationCode"]}'";
+
+            SqlCommand command = new SqlCommand(sqlQuery, _conn);
+            command.ExecuteNonQuery();
+            _conn.Close();
+        }
+    }
+}
---
+++ Session-11/ERPSolution/HRISWebApplication/Details/EmployeeProfile.aspx
@@ -0,0 +1,62 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="EmployeeProfile.aspx.cs" Inherits="HRISWebApplication.Details.EmployeeProfile" %>
+
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="asp" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        
+        <div>
+            <table style="width: 100%;">
+                <tr>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    
+                    <td colspan="3">
+                        <asp:ToolkitScriptManager ID="ScriptManager1" runat="server"></asp:ToolkitScriptManager> 
+
+                        <asp:TabContainer ID="EmployeeTabContainer" runat="server" ActiveTabIndex="2">
+                            <asp:TabPanel ID="BasicInformation" runat="server" HeaderText="Basic Information">
+                                <ContentTemplate>
+                                    <table style="width: 100%;">
+                                        <tr>
+                                            <td>&nbsp;Test</td>
+                                            <td>&nbsp;</td>
+                                            <td>&nbsp;</td>
+                                        </tr>
+                                        <tr>
+                                            <td>&nbsp;</td>
+                                            <td>&nbsp;</td>
+                                            <td>&nbsp;</td>
+                                        </tr>
+                                        <tr>
+                                            <td>&nbsp;</td>
+                                            <td>&nbsp;</td>
+                                            <td>&nbsp;</td>
+                                        </tr>
+                                    </table>
+                                </ContentTemplate>
+                            </asp:TabPanel>
+                            <asp:TabPanel ID="EducationQualification" runat="server" HeaderText="Education Qualification"></asp:TabPanel>
+                            <asp:TabPanel ID="PersonalInformation" runat="server" HeaderText="Personal Information"></asp:TabPanel>
+                        </asp:TabContainer>
+                    </td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
