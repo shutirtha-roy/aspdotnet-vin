99fdd0ef6e4f142962cb6b06e7ca677def5c4903
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-10-24 00:04:08 +0600

    Template Integrated

---
+++ Session-13/HRIS/HRIS.Web/Controllers/EmployeeLeaveController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HRIS.Web.Controllers
+{
+    public class EmployeeLeaveController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
